fcac0dfc5aff97a5edb0d5a3a2711fd9a94dc8bd
kmetelus <kmetelus@lion.lmu.edu>
2017-05-16 14:14:59 -0700

    search bar added/ images appended to result-container

--- js/main.js
+++ js/main.js
@@ -3,32 +3,32 @@ window.Select = (() => {
       init: () => {
           console.log("here");
 
-          // $.ajax({
-          //     url: './../sources/data/inventory.json',
-          //     type: 'GET',
-          //     datatype: 'json',
-          //     success: (inventory) => {
-          //         console.log("owijefoiwjef");
-          //         // inventory.forEach((item) => {
-          //         //     console.log("here");
-          //         //     $(".result-container").append($("<img src='" + item.image + "'/>"));
-          //         // });
-          //     },
-          //     error: () => {
-          //         $(".result-container").append($("<div class='col-xs-6'>ERROR</div>"));
-          //         console.log("hereeeeeee");
-          //     }
-          // });
-
-          $.getJSON("./../sources/data/inventory.json").success((inventory) => {
-              console.log("inventory");
-              inventory.forEach((item) => {
-                  console.log(item);
-                  // $(".result-container").append($("<img src='" + item.image + "'/>"));
-              });
-          }).error(() => {
-              console.log("error");
+          $.ajax({
+              url: './../sources/data/inventory.json',
+              type: 'GET',
+              datatype: 'json',
+              success: (inventory) => {
+                  // console.log("owijefoiwjef");
+                  inventory.forEach((item) => {
+                      console.log("here");
+                      $(".result-container").append($("<div><img class='item' src='" + item.image + "'/><div>"));
+                  });
+              },
+              error: () => {
+                  $(".result-container").append($("<div class='col-xs-6'>ERROR</div>"));
+                  console.log("hereeeeeee");
+              }
           });
+
+          // $.getJSON("./../sources/data/test.json").success((inventory) => {
+          //     console.log("inventory");
+          //     inventory.forEach((item) => {
+          //         console.log(item);
+          //         // $(".result-container").append($("<img src='" + item.image + "'/>"));
+          //     });
+          // }).error(() => {
+          //     console.log("error");
+          // });
       }
   }
 })();
