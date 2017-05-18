df5ddbfd266b5ec1dc58b1bb2d6c963d5b538fdc
kmetelus <kmetelus@lion.lmu.edu>
2017-05-17 17:36:12 -0700

    need to implement buttons

--- js/main.js
+++ js/main.js
@@ -1,34 +1,24 @@
 window.Select = (() => {
   return {
       init: () => {
-          console.log("here");
-
           $.ajax({
               url: './../sources/data/inventory.json',
               type: 'GET',
               datatype: 'json',
               success: (inventory) => {
-                  // console.log("owijefoiwjef");
+                  $(".result-container").append($("<div class='row' id='rc'>"));
+
                   inventory.forEach((item) => {
-                      console.log("here");
-                      $(".result-container").append($("<div><img class='item' src='" + item.image + "'/><div>"));
+                      if (item.status === "new") {
+                        $("#rc").append($("<div class='col-xs-4 item' href='#!'><img class='item-image' src='" + item.image + "'/><p class='item-name'>" + item.name + "</p><p class='item-price'>$" + item.price + "</p><div>"));
+                      }
                   });
+                  $(".result-container").append($("</div>"));
               },
               error: () => {
                   $(".result-container").append($("<div class='col-xs-6'>ERROR</div>"));
-                  console.log("hereeeeeee");
               }
           });
-
-          // $.getJSON("./../sources/data/test.json").success((inventory) => {
-          //     console.log("inventory");
-          //     inventory.forEach((item) => {
-          //         console.log(item);
-          //         // $(".result-container").append($("<img src='" + item.image + "'/>"));
-          //     });
-          // }).error(() => {
-          //     console.log("error");
-          // });
       }
   }
 })();
