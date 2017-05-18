e603147095cb9564651bfad1a1f61fec0cfe6288
kmetelus <kmetelus@lion.lmu.edu>
2017-05-18 11:30:57 -0700

    shopping cart add but no removal

--- js/item-page.js
+++ js/item-page.js
@@ -16,7 +16,8 @@ window.FillInfo = (() => {
                   $("#item-name").append($("<h2>" + item.name + "</h2>"));
                   $("#item-image").append($("<img class ='item-image' src='./../." + item.image + "'></img>"));
                   $("#item-description").append($("<p class='item-description'>" + item.description + "</p>"));
-                  $("#item-price").append($("<p>" + item.price + "</p>"));
+                  $("#item-price").append($("<p>Price: $" + item.price + "</p>"));
+                  $("#item-price").append($("<div class='priceOfItem' style='display:none;'>" + item.price + "</div>"));
 
               },
               error: () => {
